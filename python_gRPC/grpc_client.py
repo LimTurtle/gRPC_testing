7cd71e2102cf9b137bd394cc174af93a0423ab2e
LimTurtle <pigwoo98@gmail.com>
2023-01-07 14:43:17 +0900

    Python gRPC 테스트

---
+++ python_gRPC/grpc_client.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import logging
+import grpc
+import greeter_pb2
+import greeter_pb2_grpc
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = greeter_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(greeter_pb2.HelloRequest(name='you'))
+    print("Greeter client received: " + response.message)
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    run()
